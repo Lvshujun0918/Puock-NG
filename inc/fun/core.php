00742f145143b56469a105559c5210a3c86bda66
lvshujun <Lvshujun0918@163.com>
2024-03-05 15:03:49 +0800

    ✳改正一个小错误

--- inc/fun/core.php
+++ inc/fun/core.php
@@ -871,7 +871,7 @@ function pk_get_req_data(array $model)
 function pk_get_ip_region_str($ip)
 {
     if(isset($GLOBALS['ip2_region'])){
-        $ip2_instance = @$GLOBALS['ip2_region'];
+        $ip2_instance = $GLOBALS['ip2_region'];
     } else {
         $ip2_instance = new \Ip2Region();
         $GLOBALS['ip2_region'] = $ip2_instance;
