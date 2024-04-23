e529fe74137282325f1e889df72d665737f9e13f
lvshujun <Lvshujun0918@163.com>
2024-04-23 16:47:01 +0800

    ✴php74兼容适配

--- functions.php
+++ functions.php
@@ -46,9 +46,48 @@ if(!function_exists('str_starts_with')){
      * @author lvshujun
      * @date 2024-04-23
      */
-    function str_starts_with($haystack, $needle)
+    function str_starts_with(string $haystack, string $needle): bool
     {
-        return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
+        return 0 === strncmp($haystack, $needle, \strlen($needle));
+    }
+}
+
+if(!function_exists('str_ends_with')){
+    /**
+     * str_ends_with
+     *
+     * @param string $haystack 要比较的字符串
+     * @param string $needle 结尾的字符串
+     * @return bool 是否以它结尾
+     * @author lvshujun
+     * @date 2024-04-23
+     */
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ('' === $needle || $needle === $haystack) {
+            return true;
+        }
+        if ('' === $haystack) {
+            return false;
+        }
+        $needleLength = \strlen($needle);
+        return $needleLength <= \strlen($haystack) && 0 === substr_compare($haystack, $needle, -$needleLength);
+    }
+}
+
+if(!function_exists('str_contains')){
+    /**
+     * str_starts_with兼容适配
+     *
+     * @param string $haystack 父字符串
+     * @param string $needle 子字符串
+     * @return boolean 判断是否包含
+     * @author lvshujun
+     * @date 2024-04-23
+     */
+    function str_contains(string $haystack, string $needle): bool
+    {
+        return '' === $needle || false !== strpos($haystack, $needle);
     }
 }
 
