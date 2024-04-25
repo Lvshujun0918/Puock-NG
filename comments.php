d5ec2debf5e3cae0af64b209a11e0fa8fd921924
lvshujun <Lvshujun0918@163.com>
2024-04-25 13:02:20 +0800

    ✴调整验证码框样式

--- comments.php
+++ comments.php
@@ -60,7 +60,7 @@
 
                             <?php endif; ?>
                             <?php if (pk_is_checked('vd_comment') && pk_get_option('vd_type', 'img') === 'img'): ?>
-                                <div class="col-12 col-sm-3">
+                                <div class="col-12 col-sm-5">
                                     <div class="row flex-row justify-content-end">
                                         <div class="col-8 col-sm-7 text-end pl15">
                                             <input type="text" value="" placeholder="验证码" maxlength="4"
