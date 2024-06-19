367eec64f2bf65cc88840aef5836659a84ad10c2
lvshujun <Lvshujun0918@163.com>
2024-06-19 16:16:34 +0800

    ✴分块搜索代码

---
+++ src/chunk/search.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import * as common from '../common';
+
+export function searchToggle() {
+    common.web_log_push('Toggle Search');
+    //找到搜索框
+    const search = $("#search");
+    //看一下数据, 判断是否开启了
+    const open = search.attr("data-open") === "true";
+    //通过开启状态来判断动画
+    let tag = open ? 'Out' : 'In';
+    //加动画类
+    search.attr("class", "animated fade" + tag + "Left");
+    $("#search-backdrop").attr("class", "modal-backdrop animated fade" + tag + "Right");
+    //切换状态
+    search.attr("data-open", !open);
+    if (!open) {
+        //聚焦一下搜索框
+        search.find("input").trigger( "focus" );
+    }
+}
