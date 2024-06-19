695bc7567138b39e9753a1035ff10066f827cd67
lvshujun <Lvshujun0918@163.com>
2024-06-19 16:06:41 +0800

    ✴添加注释

--- src/common.js
+++ src/common.js
@@ -6,7 +6,11 @@ export function web_log_push(msg = "") {
     console.log("[Puock-NG] " + msg);
 }
 
-
+/**
+ * 返回类名称
+ * @param {string} str 类名称
+ * @returns string
+ */
 export function getClassName(str = "") {
     return intelligent_obj.name + "-" + str;
 }
